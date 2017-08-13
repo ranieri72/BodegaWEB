acd635dbf2a87c971b85a0e0352602f1ef265e92
Ranieri <ranieri72@hotmail.com>
2017-08-13 02:22:09 -0300

    Correção da função de movimentação de estoque e de outros bugs

--- app/src/main/java/com/ranieri/bodegaweb/view/LoginActivity.java
+++ app/src/main/java/com/ranieri/bodegaweb/view/LoginActivity.java
@@ -3,6 +3,7 @@ package com.ranieri.bodegaweb.view;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.Toast;
@@ -34,6 +35,8 @@ public class LoginActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
         setTitle(getResources().getString(R.string.login));
         ButterKnife.bind(this);
     }
